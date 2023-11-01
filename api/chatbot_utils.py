b0fd98e9af42769c4e422c6b0b54cd367be3d795
Alex Leventer <3254549+alexleventer@users.noreply.github.com>
2023-11-01 12:19:44 -0700

    Update chatbot_utils.py

--- api/chatbot_utils.py
+++ api/chatbot_utils.py
@@ -10,7 +10,7 @@ from langchain.embeddings import OpenAIEmbeddings
 request_url = f"https://{ASTRA_DB_ID}-{ASTRA_DB_REGION}.apps.astra.datastax.com/api/json/v1/{KEYSPACE}/{COLLECTION_NAME}"
 request_headers = { 'x-cassandra-token': ASTRA_DB_APPLICATION_TOKEN,  'Content-Type': 'application/json'}
 
-#langchain openai interface
+# langchain openai interface
 llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 embedding_model = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY) 
 
@@ -20,12 +20,11 @@ def get_similar_docs(query, number):
     embedding = list(embedding_model.embed_query(query))
     payload = json.dumps({"find": {"sort": {"$vector": embedding},"options": {"limit": number}}})
     relevant_docs = requests.request("POST", request_url, headers=request_headers, data=payload).json()['data']['documents']
-    #print(relevant_docs)
     docs_contents = [row['answer'] for row in relevant_docs] 
     docs_urls = [row['document_id'] for row in relevant_docs]
     return docs_contents, docs_urls
     
-#promt that is sent to openai using the response from the vector database and the users original query
+# prompt that is sent to openai using the response from the vector database and the users original query
 prompt_boilerplate = "Answer the question posed in the user query section using the provided context. If you don't know the answer, just say that you don't know, don't try to make up an answer. Also remark on whether the provided context was useful in generating the answer and why."
 user_query_boilerplate = "USER QUERY: {userQuery}"
 document_context_boilerplate = "CONTEXT: {documentContext}"
@@ -39,7 +38,6 @@ def build_full_prompt(query):
 
     nl = "\n"
     combined_prompt_template = PromptTemplate.from_template(prompt_boilerplate + nl + user_query_boilerplate + nl + document_context_boilerplate + nl + final_answer_boilerplate)
-    #print(combined_prompt_template)
     filled_prompt_template = combined_prompt_template.format(userQuery=query, documentContext=docs_single_string)
     print(filled_prompt_template)
     return filled_prompt_template, url
