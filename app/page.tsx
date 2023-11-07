c80d77bf6fb7f2dd90a23c306edebed5bb1599e3
Alex Leventer <3254549+alexleventer@users.noreply.github.com>
2023-11-07 12:20:35 -0800

    Remove comments

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,7 @@ import Bubble from '../components/Bubble'
 import { useChat } from 'ai/react';
 
 export default function Home() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
-    onError: (err) => console.log(err),
-    onResponse: (res) => {
-      console.log("RES");
-      console.log(res);
-    },
-    onFinish: (fsh) => {
-      console.log("FISH");
-      console.log(fsh)
-    }
-  });
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   const messagesEndRef = useRef(null);
 
