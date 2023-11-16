92717beede69c9073244073e1387c8a079726c28
Mike Fortman <michael.fortman@datastax.com>
2023-11-16 12:44:14 -0600

    fix server vs client mismatch

--- components/ThemeButton.tsx
+++ components/ThemeButton.tsx
@@ -1,34 +1,45 @@
 import { useEffect, useState } from "react";
 
 const ToggleButton = () => {
-  const getLocalValue = (key: string, defaultValue: any) => {
-    if (typeof window !== 'undefined') {
-      const storedValue = localStorage.getItem(key);
+  const [theme, setTheme] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getLocalValue = () => {
+      const storedValue = localStorage.getItem('theme');
       if (storedValue !== null) {
         return storedValue;
       }
-      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-    }
-    return defaultValue;
-  };
+      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    };
 
-  const [theme, setTheme] = useState<string>(() => getLocalValue('theme', 'dark'));
+    const initialTheme = getLocalValue();
+    setTheme(initialTheme);
 
-  // Persist to localStorage
-  useEffect(() => {
-    if (theme === 'dark') {
+    if (initialTheme === 'dark') {
       document.documentElement.classList.add('dark');
     } else {
       document.documentElement.classList.remove('dark');
     }
-    if (typeof window !== 'undefined') {
+  }, []);
+
+  useEffect(() => {
+    if (theme !== null) {
       localStorage.setItem('theme', theme);
+      if (theme === 'dark') {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
     }
   }, [theme]);
 
   const handleToggle = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark');
   };
+
+  if (theme === null) {
+    return null;
+  }
 
   return (
     <button onClick={handleToggle}>
